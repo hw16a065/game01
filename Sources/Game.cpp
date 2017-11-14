f9419a436c9e92dcfa8475fa33abc754f54ff99c
hw16a065 <hw16a065@oecu.jp>
2017-11-14 16:39:58 +0900

    コミットしました

--- Sources/Game.cpp
+++ Sources/Game.cpp
@@ -1,6 +1,5 @@
 #include "Game.hpp"
 
-
 // TODO: 砲台の位置を画面左に、ターゲットの位置を画面右に移動させる。(A)
 // TODO: 雲の位置を左から右に動かす。見えなくなったら左端に戻す。(B)
 // TODO: 砲台を青い壁に沿って上下に動かす。(C)
@@ -17,67 +16,74 @@ Vector2 bulletPos;      //!< 弾の位置
 Rect    targetRect;     //!< ターゲットの矩形
 int     score;          //!< スコア
 
-
 // ゲーム開始時に呼ばれる関数です。
 void Start()
 {
-    cloudPos = Vector2(-320, 100);
-    cannonPos = Vector2(-80, -150);
-    targetRect = Rect(80, -140, 40, 40);
-    bulletPos.x = -999;
-    score = 0;
-    PlayBGM("bgm_maoudamashii_8bit07.mp3");
-}
+        cloudPos = Vector2(-320, 100);
+        cannonPos = Vector2(-80, -150);
+        targetRect = Rect(80, -140, 40, 40);
+        bulletPos.x = -999;
+        score = 0;
+        PlayBGM("bgm_maoudamashii_8bit07.mp3");
+    }
 
 void PlaySE(){
-    PlaySound("se_maoudamashii_explosion03.mp3");
-}
+        PlaySound("se_maoudamashii_explosion03.mp3");
+    }
 
 // 1/60秒ごとに呼ばれる関数です。モデルの更新と画面の描画を行います。
 void Update()
 {
     
-    // 弾の発射
-    if (bulletPos.x <= -999 && Input::GetKeyDown(KeyMask::Space)) {
-        bulletPos = cannonPos + Vector2(50, 10);
-        PlaySE();
+        // 弾の発射
+        if (bulletPos.x <= -999 && Input::GetKeyDown(KeyMask::Space)) {
+                bulletPos = cannonPos + Vector2(50, 10);
+                PlaySE();
+            }
+    
+        // 弾の移動
+        if (bulletPos.x > -999) {
+                bulletPos.x += 10 * Time::deltaTime;
+        
+                // ターゲットと弾の当たり判定
+                Rect bulletRect(bulletPos, Vector2(32, 20));
+                if (targetRect.Overlaps(bulletRect)) {
+                        score += 100;         // スコアの加算
+                        bulletPos.x = -999; // 弾を発射可能な状態に戻す
+                        PlaySound("se_maoudamashii_explosion06.mp3");
+                    }
+            }
+    
+        // 背景の描画
+        Clear(Color::cyan);
+        FillRect(Rect(-320, -240, 640, 100), Color::green);
+    
+        // 雲の描画
+        DrawImage("cloud1.png", cloudPos);
+    
+        // 弾の描画
+        if (bulletPos.x > -999) {
+                DrawImage("bullet.png", bulletPos);
+            }
+    
+        // 砲台の描画
+        FillRect(Rect(cannonPos.x-10, -140, 20, 100), Color::blue);
+        DrawImage("cannon.png", cannonPos);
+    
+        // ターゲットの描画
+        FillRect(targetRect, Color::red);
+    
+        // スコアの描画
+        SetFont("nicoca_v1.ttf", 20.0f);
+        DrawText(FormatString("%05d", score), Vector2(-319, 199), Color::black);
+        DrawText(FormatString("%05d", score), Vector2(-320, 200), Color::white);
     }
+//
+//  Game.cpp
+//  Game
+//
+//  Created by hw16a065 on 2017/11/14.
+//  Copyright © 2017年 Satoshi Numata. All rights reserved.
+//
 
-    // 弾の移動
-    if (bulletPos.x > -999) {
-        bulletPos.x += 10 * Time::deltaTime;
-
-        // ターゲットと弾の当たり判定
-        Rect bulletRect(bulletPos, Vector2(32, 20));
-        if (targetRect.Overlaps(bulletRect)) {
-            score += 100;         // スコアの加算
-            bulletPos.x = -999; // 弾を発射可能な状態に戻す
-            PlaySound("se_maoudamashii_explosion06.mp3");
-        }
-    }
-
-    // 背景の描画
-    Clear(Color::cyan);
-    FillRect(Rect(-320, -240, 640, 100), Color::green);
-
-    // 雲の描画
-    DrawImage("cloud1.png", cloudPos);
-
-    // 弾の描画
-    if (bulletPos.x > -999) {
-        DrawImage("bullet.png", bulletPos);
-    }
-
-    // 砲台の描画
-    FillRect(Rect(cannonPos.x-10, -140, 20, 100), Color::blue);
-    DrawImage("cannon.png", cannonPos);
-
-    // ターゲットの描画
-    FillRect(targetRect, Color::red);
-
-    // スコアの描画
-    SetFont("nicoca_v1.ttf", 20.0f);
-    DrawText(FormatString("%05d", score), Vector2(-319, 199), Color::black);
-    DrawText(FormatString("%05d", score), Vector2(-320, 200), Color::white);
-}
-
+#include "Game.hpp"
